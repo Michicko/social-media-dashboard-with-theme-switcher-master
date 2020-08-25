c885a43eeeccb7e5f027e36db8f4e70a589142d2
Otaigbe <michicko81@gmail.com>
2020-08-26 00:02:45 +0100

    some details

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 const toggle = document.querySelector('.header__checkbox');
+const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
 
 
 toggle.addEventListener('change', switchMode);
@@ -6,7 +7,17 @@ toggle.addEventListener('change', switchMode);
 function switchMode(e) {
     if (e.target.checked) {
         document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
     } else {
         document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
+    }
+}
+
+// check for saved theme
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+    if (currentTheme === 'light') {
+        toggle.checked = true;
     }
 }

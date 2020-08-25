17844e4bdcb5181a409cbb1f349544e09ff87b60
Otaigbe <michicko81@gmail.com>
2020-08-26 00:19:43 +0100

    details

--- js/app.js
+++ js/app.js
@@ -1,23 +1,47 @@
 const toggle = document.querySelector('.header__checkbox');
-const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
 
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    let currentTheme = getTheme();
+    if (currentTheme === 'light') {
+        toggle.checked = true;
+        document.documentElement.setAttribute('data-theme', 'light');
+        saveTheme('light');
+    } else {
+        toggle.checked = false;
+    }
+});
 
 toggle.addEventListener('change', switchMode);
 
+// switch between dark and light
 function switchMode(e) {
     if (e.target.checked) {
         document.documentElement.setAttribute('data-theme', 'light');
-        localStorage.setItem('theme', 'light');
+        saveTheme('light');
     } else {
         document.documentElement.setAttribute('data-theme', 'dark');
-        localStorage.setItem('theme', 'dark');
+        saveTheme('dark');
     }
 }
 
-// check for saved theme
-if (currentTheme) {
-    document.documentElement.setAttribute('data-theme', currentTheme);
-    if (currentTheme === 'light') {
-        toggle.checked = true;
+// get saved theme
+function getTheme() {
+    let theme;
+    if (localStorage.getItem('theme') === null) {
+        theme = null;
+    } else {
+        theme = localStorage.getItem('theme');
     }
+    return theme;
+}
+
+
+// save theme to storage
+function saveTheme(mode) {
+    let theme = getTheme();
+    theme = mode;
+    localStorage.setItem('theme', theme);
 }
